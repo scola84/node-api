825aafb630dde8672a046c15b7a912f771456b09
M.C. van den Boogaart <scola@kvdb.name>
2016-09-29 23:47:04 +0200

    Update dependencies and scripts

--- rollup.config.js
+++ rollup.config.js
@@ -5,34 +5,14 @@ import globals from 'rollup-plugin-node-globals';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
+  dest: './dist/api.js',
   entry: 'index.js',
   format: 'umd',
-  globals: {
-    // 'querystring': 'querystring',
-    // 'stream': 'stream',
-    // 'url': 'url',
-    // 'string_decoder': 'string_decoder'
-
-    // 'd3-selection': 'd3',
-    // 'd3-transition': 'd3',
-    // 'moment': 'moment',
-    // 'moment-timezone': 'moment'
-  },
+  moduleName: 'api',
   plugins: [
     builtins(),
     resolve({
-      jsnext: true,
-      skip: [
-        // 'querystring',
-        // 'stream',
-        // 'url',
-        // 'string_decoder'
-
-        // 'd3-selection',
-        // 'd3-transition',
-        // 'moment',
-        // 'moment-timezone'
-      ]
+      jsnext: true
     }),
     commonjs({
       ignoreGlobal: true
